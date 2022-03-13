e2b0f240707fe6a18482ac8e83d3f31b6536f883
Russell Helmstedter <rhelmstedter@gmail.com>
2022-03-13 13:33:47 -0700

    Converted to pure lua

--- nvim/.config/nvim/lua/helm/python.lua
+++ nvim/.config/nvim/lua/helm/python.lua
@@ -1,35 +1,44 @@
-vim.cmd([[
-    " for WSL on work comp
-    " let g:python3_host_prog= '/usr/bin/python3'
+-- for WSL on work comp
+-- vim.g.python3_host_prog= '/usr/bin/python3'
 
-    " for mac-mini
-    let g:python3_host_prog= '/opt/homebrew/bin/python3'
+-- for mac-mini
+-- vim.g.python3_host_prog= '/opt/homebrew/bin/python3'
 
-    " for mbp 2015
-    " let g:python3_host_prog= '/usr/local/bin/python3'
+-- for mbp 2015
+vim.g.python3_host_prog = "/usr/local/bin/python3"
 
-    " f-strings
-    nnoremap <leader>fs maF"if<esc>`al
+-- vim cell-mode parameters
+vim.g.cellmode_use_tmux = 1
+vim.g.cellmode_tmux_panenumber = 0
+vim.g.VimuxRunnerType = "window"
+vim.g.cellmode_tmux_windowname = "Python"
 
-    augroup formatPython
-        autocmd!
-        autocmd BufWritePre *.py execute ':Black'
-        autocmd BufWritePre *.py execute ':Isort'
-    augroup END
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
 
-    " vim cell-mode parameters
-    let g:cellmode_use_tmux=1
-    let g:cellmode_tmux_panenumber=0
-    let g:VimuxRunnerType = "window"
-    let g:cellmode_tmux_windowname='Python'
+local format_python = augroup("format_python", { clear = true })
+autocmd("BufWritePre", {
+    pattern = "*.py",
+    command = "Black",
+    group = format_python,
+})
+autocmd("BufWritePre", {
+    pattern = "*.py",
+    command = "Isort",
+    group = format_python,
+})
 
-    " ipython-shell
-    noremap <leader>ss :call StartPyShell()<CR>
-    noremap <leader>sk :call StopPyShell()<CR>
+local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
 
-    " mode execution
-    nnoremap <leader>sl  :call PyShellSendLine()<CR>
-    nnoremap <leader>rc :call RunTmuxPythonCell(0)<CR>
-    noremap <leader>ra :call RunTmuxPythonAllCellsAbove()<CR>
+-- f-strings
+keymap("n", "<leader>fs", 'maF"if<esc>`al', opts)
 
-]])
+-- ipython-shell
+keymap("n", "<leader>ss", "<cmd> call StartPyShell()<CR>", opts)
+keymap("n", "<leader>sk", "<cmd> call StopPyShell()<CR>", opts)
+
+-- mode execution
+keymap("n", "<leader>sl", "<cmd> call PyShellSendLine()<CR>", opts)
+keymap("n", "<leader>rc", "<cmd> call RunTmuxPythonCell(0)<CR>", opts)
+keymap("n", "<leader>ra", "<cmd> call RunTmuxPythonAllCellsAbove()<CR>", opts)
