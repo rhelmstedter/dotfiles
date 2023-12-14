982e8357d40e4c309a744e6f571e2ef955d65eb3
Russell Helmstedter <rhelmstedter@gmail.com>
2023-12-13 19:05:23 -0800

    updated roam dir

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -118,7 +118,6 @@
 (eval-after-load 'ox '(add-to-list
                        'org-export-filter-timestamp-functions
                        'org-export-filter-timestamp-remove-brackets))
-
 (after! org
   (set-face-attribute 'org-level-1 nil
                       :height 1.07
@@ -133,7 +132,7 @@
                       :height 1.10
                       :weight 'bold))
 
-(setq org-roam-directory "~/Dropbox/roam")
+(setq org-roam-directory "~/Dropbox/org/roam")
 ;; Roam Graph
 (use-package! websocket
   :after org-roam)
@@ -148,6 +147,53 @@
         org-roam-ui-follow t
         org-roam-ui-update-on-save t
         org-roam-ui-open-on-start nil))
+
+;; Latex
+(with-eval-after-load 'ox-latex
+(add-to-list 'org-latex-classes
+             '("org-plain-latex"
+               "\\documentclass{article}
+           [NO-DEFAULT-PACKAGES]
+           [PACKAGES]
+           [EXTRA]"
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+               ("\\paragraph{%s}" . "\\paragraph*{%s}")
+               ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))))
+
+;; custom time stamp heading
+;; ====================
+;; insert date and time
+
+;; (defvar current-date-time-format "%a %b %d %H:%M:%S %Z %Y"
+;;   "Format of date to insert with `insert-current-date-time' func
+;; See help of `format-time-string' for possible replacements")
+
+(defvar current-time-format "%H:%M"
+  "Format of date to insert with `insert-current-time' func.
+Note the weekly scope of the command's precision.")
+
+;; (defun insert-current-date-time ()
+;;   "insert the current date and time into current buffer.
+;; Uses `current-date-time-format' for the formatting the date/time."
+;;        (interactive)
+;;        (insert "*** ")
+;; ;       (insert (let () (comment-start)))
+;;        (insert (format-time-string current-date-time-format (current-time)))
+;;        (insert "\n")
+;;        )
+
+(defun insert-current-time ()
+  "insert the current time (1-week scope) into the current buffer."
+       (interactive)
+       (insert "**** ")
+       (insert (format-time-string current-time-format (current-time)))
+       (insert "\n")
+       )
+
+(global-set-key "\C-c\C-d" 'insert-current-date-time)
+(global-set-key (kbd "\C-c t") 'insert-current-time)
 
 ;; (org-roam-capture-templates
 ;;  '(("d" "default" plain
