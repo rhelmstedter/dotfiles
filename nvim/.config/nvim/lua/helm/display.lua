56e65b38cb4cec0c32a956dae2adc55f62fd4630
Russell Helmstedter <rhelmstedter@gmail.com>
2024-12-06 20:12:06 -0800

    changed colorscheme

--- nvim/.config/nvim/lua/helm/display.lua
+++ nvim/.config/nvim/lua/helm/display.lua
@@ -1,38 +1,38 @@
-require('hlargs').setup()
-
--- Add color to cursor
-vim.g.doom_one_cursor_coloring = true
--- terminal colors
-vim.g.doom_one_terminal_colors = true
--- Enable italic comments
-vim.g.doom_one_italic_comments = true
--- Enable TS support
-vim.g.doom_one_enable_treesitter = true
--- Color whole diagnostic text or only underline
-vim.g.doom_one_diagnostics_text_color = false
--- Enable transparent background
-vim.g.doom_one_transparent_background = false
-
--- Pumblend transparency
-vim.g.doom_one_pumblend_enable = false
-vim.g.doom_one_pumblend_transparency = 20
-
--- Plugins integration
-vim.g.doom_one_plugin_nvim_tree = false
-vim.g.doom_one_plugin_whichkey = false
-vim.g.doom_one_plugin_indent_blankline = false
-vim.g.doom_one_plugin_lspsaga = false
-vim.g.doom_one_plugin_barbar = false
-vim.g.doom_one_plugin_neorg = false
-vim.g.doom_one_plugin_telescope = true
-vim.g.doom_one_plugin_neogit = false
-vim.g.doom_one_plugin_dashboard = false
-vim.g.doom_one_plugin_startify = false
-vim.g.doom_one_plugin_vim_illuminate = false
-
-vim.cmd([[
-colorscheme doom-one
-]])
+-- require('hlargs').setup()
+--
+-- -- Add color to cursor
+-- vim.g.doom_one_cursor_coloring = true
+-- -- terminal colors
+-- vim.g.doom_one_terminal_colors = true
+-- -- Enable italic comments
+-- vim.g.doom_one_italic_comments = true
+-- -- Enable TS support
+-- vim.g.doom_one_enable_treesitter = true
+-- -- Color whole diagnostic text or only underline
+-- vim.g.doom_one_diagnostics_text_color = false
+-- -- Enable transparent background
+-- vim.g.doom_one_transparent_background = false
+--
+-- -- Pumblend transparency
+-- vim.g.doom_one_pumblend_enable = false
+-- vim.g.doom_one_pumblend_transparency = 20
+--
+-- -- Plugins integration
+-- vim.g.doom_one_plugin_nvim_tree = false
+-- vim.g.doom_one_plugin_whichkey = false
+-- vim.g.doom_one_plugin_indent_blankline = false
+-- vim.g.doom_one_plugin_lspsaga = false
+-- vim.g.doom_one_plugin_barbar = false
+-- vim.g.doom_one_plugin_neorg = false
+-- vim.g.doom_one_plugin_telescope = true
+-- vim.g.doom_one_plugin_neogit = false
+-- vim.g.doom_one_plugin_dashboard = false
+-- vim.g.doom_one_plugin_startify = false
+-- vim.g.doom_one_plugin_vim_illuminate = false
+--
+-- -- vim.cmd([[
+-- colorscheme doom-one
+-- ]])
 -- require("doom-one").setup {
 --     cursor_coloring = false,
 --     terminal_colors = true,
